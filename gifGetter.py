4ed28c0339419056a70d3a8f0bbca1b0cba1706c
Corey Miller <cgm47@zips.uakron.edu>
2019-03-03 10:24:50 -0500

    Allow gifs to open in browser window

--- gifGetter.py
+++ gifGetter.py
@@ -1,18 +1,22 @@
 import requests
 import json 
 import random
+import urllib
+import webbrowser
 
-#api_key = "szq7jGYSNk7o9nHttekOuD6busT5MmKs"
-#query = "angry"
-#numberOfResults = 5
-#
-#response = requests.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + api_key + "&limit=" + str(numberOfResults))
-#
-#if response.status_code != 200:
-#    print("Request went wrong :/")
-#else: 
-#    data = json.loads(response.content)
-#    print(json.dumps(data, sort_keys=True, indent=4))
-#    print(data['data'][1]['url'])
+q = "angry"
 
-print(random.randint(0,numberOfResults))
+def getGif(query):
+    api_key = "szq7jGYSNk7o9nHttekOuD6busT5MmKs"
+    numberOfResults = 5
+    
+    response = requests.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + api_key + "&limit=" + str(numberOfResults))
+    
+    if response.status_code != 200:
+        print("Request went wrong :/")
+    else: 
+        data = json.loads(response.content)
+        index = random.randint(0,numberOfResults - 1)
+        gifLocation = data['data'][index]['embed_url']
+        webbrowser.open(gifLocation)
+
